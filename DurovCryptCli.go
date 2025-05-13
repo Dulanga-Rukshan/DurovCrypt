0c060b7265edada6c67c8b34c2d510ffaf2bfc07
Dulanga-Rukshan <dulangarukshan@proton.me>
2025-05-14 00:27:53 +0530

    -F-

--- DurovCryptCli.go
+++ DurovCryptCli.go
@@ -5,94 +5,58 @@ import (
 	"fmt"
 
 	// "os"
-	"runtime"
 	"strings"
+)
+
+const (
+	encryptFunctionCall = "Encrypt"
+	decryptFunctionCall = "Decrypt"
 )
 
 func main() {
 
-	//password policy
-	Policy := ess.PasswordPolicyCheck{
-		MinLength:           6,
-		MaxLength:           64,
-		RequireUpper:        true,
-		RequireLower:        true,
-		RequireSymbol:       true,
-		RequireNumber:       true,
-		DenyWhiteSpace:      true,
-		AllowedSpecialChars: "{}()!@#$%^&*+_<>?:';,.][-|\"\\/",
-	}
-
 	//Welcome message
 
-	// args := os.Args
+	//args := os.Args
 
 	// if len(args) > 1{
 	// 	if
 	// }
 	//ask user for what to do
-	Operation, err1 := ess.InputPrompt("What operation do you wanna perform: ")
-	if err1 != nil {
-		fmt.Printf("ERR: Error getting input prompt. \n\n%v", err1)
-
-	}
+	Operation, err1 := ess.DefaultAskInput("What operation do you wanna perform: ")
+	ess.MainErr(err1)
 
 	switch {
 
 	//if user input 'e' for encrypt
-	case strings.ToUpper(Operation) == "E":
-
+	case strings.ToUpper(Operation) == "E" || strings.ToUpper(Operation) == "ENCRYPT":
 		//getting file path for encrypt
-		fileName, err2 := ess.FilePathInput()
-		if err2 != nil {
-			fmt.Printf("\nERROR: %v\n", err2) // Prints the error from IsValidFileName
-			return
-		}
+		fileName, err2 := ess.FilePathInput(encryptFunctionCall)
+		ess.MainErr(err2)
 
 		//ask user for password to encrypt
-		password, err3 := ess.InputPrompt("Enter a Password for encrypt: ")
-		if err3 != nil {
-			fmt.Printf("\nERROR: %v\n", err3)
-			return
-		}
-		err4 := ess.PasswordChecker(password, Policy)
-		if err4 != nil {
-			fmt.Printf("\nERROR: %v\n", err4)
-			return
-		}
+		password, err3 := ess.PasswordAskInput(encryptFunctionCall)
+		ess.MainErr(err3)
 
 		//key generation
-		salt := ess.DataKey()
-		NewKey := &ess.Aragon2Key{
-			Password:  []byte(password),
-			Salt:      salt,
-			Iteration: uint32(3),
-			MemSize:   uint32(64 * 1024),
-			Threads:   uint8(runtime.NumCPU()),
-			KeyLength: 32,
-		}
+		derivedKey, err5 := ess.KeyGen(password, encryptFunctionCall)
+		ess.MainErr(err5)
 
-		derivedKey, err6 := NewKey.Generate()
-		if err6 != nil {
-			fmt.Printf("\nERROR: %v\n", err6)
-			return
-		}
-
-		//open the file and read it
-		fileData, err7 := ess.FileRead(fileName)
-		if err7 != nil {
-			fmt.Printf("\nERROR: %v\n", err7)
-			return
-		}
+		//open the file and read the data and assign data to variable
+		fileData, err6 := ess.FileRead(fileName)
+		ess.MainErr(err6)
 
 		//encrypting the data
-		encryptedData, err8 := ess.Encrypt(derivedKey, string(fileData))
-		if err8 != nil {
-			fmt.Printf("\nERR: %v\n", err8)
-			return
-		}
-		fmt.Print(encryptedData)
-		//
-	}
+		encryptedData, err7 := ess.Encrypt(derivedKey, string(fileData))
+		ess.MainErr(err7)
 
+		//write the ciphertext data to file
+		successMsg, err8 := ess.FileWrite([]byte(encryptedData), fileName)
+		ess.MainErr(err8)
+
+		fmt.Printf("File Encrypted. %v.", successMsg)
+
+		// case strings.ToUpper(Operation) == "D" || strings.ToUpper(Operation) == "DECRYPT":
+		// 	OperationForEnAndDe(decryptFunctionCall)
+	}
 }
